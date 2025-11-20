4bc88e0944c6e43c574a6b7473366be717529267
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2025-11-20 10:47:58 +0100

    xfgj

--- VettoriParalleliFunzioni/Program.cs
+++ VettoriParalleliFunzioni/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+        static void stampa_inventario( string[] prodotti, int[] quantità)
+        {
+            for(int i = 0; i < prodotti.Length; i++)
+            {
+                Console.Write(prodotti[i] + "   ");
+            }
+
+            Console.WriteLine("    ");
+
+            for (int i = 0; i < quantità.Length; i++)
+            {
+                Console.Write(quantità[i] + "       ");
+            }
+        }
+        static string cerca_libro()
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] prodotti = { "mela", "banana", "arancia", "uva" };
+
+            int[] quantità = { 10, 5, 12, 34 };
+
+            stampa_inventario(prodotti, quantità); 
         }
     }
 }
